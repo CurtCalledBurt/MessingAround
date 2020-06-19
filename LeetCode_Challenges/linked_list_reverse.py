77d46a15df57638b35e19d1c5a5ef4cb86ed47b3
CurtCalledBurt <CurtCalledBurt@users.noreply.github.com>
2020-06-19 14:58:57 -0700

    functionized linked list creation and reversal

--- LeetCode_Challenges/linked_list_reverse.py
+++ LeetCode_Challenges/linked_list_reverse.py
@@ -4,36 +4,42 @@ class Node():
         self.next = None
         self.value = value
 
-# create linked list
-head_node = Node(0)
-current = head_node
-for i in range(1,5):
-    current.next = Node(i)
-    current = current.next
+# create linked list from an array
+def create_linked_list(array):
+    head_node = Node(array[0])
+    current = head_node
+    for i in range(1, len(array)):
+        current.next = Node(array[i])
+        current = current.next
+    return head_node
 
-# print values of linked list
-# current = head_node
-# while current:
-#     print(current.value)
-#     current = current.next
+# print values of nodes in linked list
+def print_linked_list(head_node):
+    current = head_node
+    while current:
+        print(current.value)
+        current = current.next
+
+array = [1,2,3,4,5]
+
+head_node = create_linked_list(array)
 
 # reverse a linked list
-new_head = head_node
-current = new_head
-prev = None
+def reverse_linked_list(head_node):
+    new_head = head_node
+    current = new_head
+    prev = None
 
-while current:
-    # save the next node
-    nxt = current.next
-    current.next = prev
-    prev = current
-    current = nxt
-    if current is None:
-        new_head = prev
+    while current:
+        # save the next node
+        nxt = current.next
+        current.next = prev
+        prev = current
+        current = nxt
+        if current is None:
+            new_head = prev
+    return new_head
 
+new_head = reverse_linked_list(head_node)
 
-# print reversed linked list
-current = new_head
-while current:
-    print(current.value)
-    current = current.next
+print_linked_list(new_head)
