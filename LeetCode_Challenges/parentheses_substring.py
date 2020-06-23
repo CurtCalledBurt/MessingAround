4b9d9389d094c6c50c52c2837f0fe9e7e6a65f5d
CurtCalledBurt <CurtCalledBurt@users.noreply.github.com>
2020-06-23 16:59:30 -0700

    few fixes to base function

--- LeetCode_Challenges/parentheses_substring.py
+++ LeetCode_Challenges/parentheses_substring.py
@@ -8,15 +8,17 @@ def longest_parenth_substring(string):
     length = 0
     for i in range(len(string)):
         if string[i] == ")":
-            if prev == "(":
+            if prev == "(": # complete pareth pair found, increase length of current substring by 2
                 length += 2
                 if length > max_length:
                     max_length = length
-            else:
+            else: # invalid pareth pair, start length over from 0
                 length = 0
         if string[i] == "(":
-            if prev == "(":
+            if prev == "(": # invalid pareth pair, starth length over from 0
                 length = 0
+            else: # potential continuation of valid substring,
+                pass
 
         prev = string[i]
 
