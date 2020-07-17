5e8bf9de9095638e7dd190858b0c7a9632248c89
CurtCalledBurt <CurtCalledBurt@users.noreply.github.com>
2020-07-16 17:04:11 -0700

    deprecated error bound approach in favor of directly setting the number of iterations.

--- Mathematics/farey_addition.py
+++ Mathematics/farey_addition.py
@@ -10,11 +10,10 @@
 
 # replace old left/right fraction with new fraction.
 
-# repeat until we have an acceptable level of error
+# repeat as many times as we say to repeat the process
 
 
-decimal = 0.3333333333
-error_bound = (1/10)**(12)
+decimal = 0.3493827202
 approximation = 1000000000000
 approx_numerator = 0
 approx_denominator = 1
@@ -25,7 +24,9 @@ left_denominator = 1
 right_numerator = 1
 right_denominator = 1
 
-while abs(approximation - decimal) > error_bound:
+iterations = 1_000_000
+
+for _ in range(iterations):
     new_numerator = left_numerator + right_numerator
     new_denominator = left_denominator + right_denominator
 
@@ -36,13 +37,7 @@ while abs(approximation - decimal) > error_bound:
     else:
         left_numerator = new_numerator
         left_denominator = new_denominator
-    
-    print("left fraction: ")
-    print(left_numerator, left_denominator)
-    print("right fraction: ")
-    print(right_numerator, right_denominator)
-    
-    # update approximation with closer fraction
+
     if abs(approximation - left_numerator/left_denominator) < abs(approximation - right_numerator/right_denominator):
         approximation = left_numerator/left_denominator
         approx_numerator = left_numerator
@@ -51,9 +46,10 @@ while abs(approximation - decimal) > error_bound:
         approximation = right_numerator/right_denominator
         approx_numerator = right_numerator
         approx_denominator = right_denominator
-    
-    # print(approx_numerator, approx_denominator)
 
-print(approximation)
+
+print(decimal)
+print(approximation, '\n')
 print(approx_numerator)
+print('-----------')
 print(approx_denominator)
